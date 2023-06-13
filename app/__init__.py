a0163dd85cf285c11b95f38a29e71c0671193435
oezeb <eouedraogo4@gmai.com>
2023-06-13 09:23:10 +0000

    add DB_PORT to app config

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY=secrets.token_hex(),
         DB_HOST=os.environ.get('DB_HOST'),
+        DB_PORT=os.environ.get('DB_PORT'),
         DATABASE=os.environ.get('DATABASE'),
         DB_USER=os.environ.get('DB_USER'),
         DB_PASSWORD=os.environ.get('DB_PASSWORD'),
